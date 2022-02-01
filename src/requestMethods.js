a70aabe0048b1fd6bc151e862a95b8049adc84ab
unknown <bikeshbitbucket>
2022-02-01 14:31:42 +0530

    make it better

--- src/requestMethods.js
+++ src/requestMethods.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL ="https://myssticoshopping.herokuapp.com/api"
+const BASE_URL ="https://myssticobackend.herokuapp.com/api"
 const TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYxZjM3MjU2ZTU3YjM0NjE4NDAyMmY4NiIsImlzQWRtaW4iOnRydWUsImlhdCI6MTY0MzYzMzQ4NSwiZXhwIjoxNjQzODkyNjg1fQ.at-Ib6js2upOBW92wEA_W0WoPW2mKfYChrcbVn7cpgE";
 
  export const publicRequest = axios.create({
